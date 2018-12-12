fd24bafacbb3f3093e3dcae1190d595e86b78071
Mehran Memarnejad <memarnejadm74@gmail.com>
2018-12-12 19:03:29 +0330

    1 converts to I passed

--- ArabicToRomanNumeralsConverterTestCase.cpp
+++ ArabicToRomanNumeralsConverterTestCase.cpp
@@ -1,6 +1,7 @@
 //be nam _e_ khoda
 
 #include "gtest/gtest.h"
+#include <string>
 
 int main(int argc, char* argv[]) {
 
@@ -8,6 +9,14 @@ int main(int argc, char* argv[]) {
     return RUN_ALL_TESTS();
 }
 
+std::string convertArabicNumberToRomanNumeral(const unsigned int arabicNumber){
+    return "I";
+}
+
 TEST(ArabicToRomanNumeralsConverterTestCase, PreparationsCompleted){
     GTEST_SUCCEED();
+}
+
+TEST(ArabicToRomanNumeralsConverterTestCase, 1_isConvertedTo_I){
+    ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
 }
