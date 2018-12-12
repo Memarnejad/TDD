0d2781a7f9fee31207d0d85033bc69c9fa034cd3
Mehran Memarnejad <memarnejadm74@gmail.com>
2018-12-12 18:54:34 +0330

    Project initialization completed

---
+++ ArabicToRomanNumeralsConverterTestCase.cpp
@@ -0,0 +1,13 @@
+//be nam _e_ khoda
+
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(ArabicToRomanNumeralsConverterTestCase, PreparationsCompleted){
+    GTEST_FAIL();
+}
