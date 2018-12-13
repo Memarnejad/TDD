7eb7c3eca92e9ef8b986b33823b375bde344d99b
Mehran Memarnejad <memarnejadm74@gmail.com>
2018-12-13 15:02:13 +0330

    more conversion added - WE DRIVED TOO FAST :) - Do not do that in TDD

--- ArabicToRomanNumeralsConverterTestCase.cpp
+++ ArabicToRomanNumeralsConverterTestCase.cpp
@@ -8,13 +8,20 @@ struct ArabicToRomanMapping{
     std::string romanNumeral;
 };
 
-const std::size_t numberOfMappings = 6;
+const std::size_t numberOfMappings = 13;
 using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
 
 const ArabicToRomanMappings arabicToRomanMappings{{
     {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
     {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
     {10, "X"},
+    {9, "IX"},
     {5, "V"},
     {4, "IV"},
     {1, "I"}
